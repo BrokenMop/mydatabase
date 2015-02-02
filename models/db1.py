e9533392ebd9819f1262c5ff4898f03d6c7a99ca
Jerry Liu <wenhaoliu@Jerrys-MacBook-Pro.local>
2015-02-01 21:42:18 -0800

    First commit

---
+++ models/db1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+db.define_table('my_order',
+                Field('item'),
+                Field('buyer'),
+                Field('recipient'),
+                Field('ship_date', 'date'),
+                Field('if_pass_customs','boolean'),
+                Field('receive_date', 'date'),
+                Field('if_confirmed','boolean'),
+                Field('ship_number'),
+                Field('order_destination'),
+                Field('order_comment'),
+                Field('rrp', 'double'),
+                Field('tax_rate', 'double'),
+                Field('ship_fee', 'double'),
+                Field('if_double_point','boolean')
+               )

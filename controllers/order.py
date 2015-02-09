9308b026cf9bef2d4145c8afbc18bcd856dd158c
Jerry Liu <jerryliu0228@gmail.com>
2015-02-09 14:55:21 -0600

    Implement order save

--- controllers/order.py
+++ controllers/order.py
@@ -13,7 +13,6 @@ def new():
     response.files.insert(0,URL('static','css/pure-form.css'))
     rewardOptions = selectAllRewardOptions()
 
-
     data= urllib.urlopen("http://rate-exchange.appspot.com/currency?from=USD&to=CNY").read()
     localtime   = time.localtime()
     timeString  = time.strftime("%m/%d/%Y %H:%M:%S", localtime)
@@ -21,11 +20,34 @@ def new():
     exchangeRate = "{0:.2f}".format(result['rate'])
     localtime   = time.localtime()
 
-
     return dict(message="hello from order.py",
                 rewardOptions = rewardOptions,
                 exchangeRate= exchangeRate,
                 exchangeRatePulledDate = timeString)
 
 def save():
-    return dict(message="hello from order.py")
+    customerName = request.vars['customer-name']#
+    recipientName = request.vars['recipient-name']#
+    comment = request.vars['comment']#
+    item = request.vars['item'] #
+    quantity = request.vars['quantity']#
+    giftsInfo = request.vars['gifts-info']#
+    price = request.vars['sold-price']#
+    rrp = request.vars['rrp']#
+    shipRate = request.vars['ship-rate']#
+    taxRate = request.vars['tax-rate']#
+    exchangeRate = request.vars['exchange-rate']#
+    shipDate = request.vars['ship-date']#
+    shipCompany = request.vars['ship-company']#
+    tracking = request.vars['tracking']#
+    destination = request.vars['destination']#
+    reward = getRewardWithPercentage(request.vars['reward'])#
+
+
+    db.my_order.insert(item=item, quantity=quantity, gifts_info=giftsInfo, customer_name=customerName,
+                       recipient_name=recipientName, ship_date=shipDate, ship_compony=shipCompany,
+                       tracking=tracking, order_destination=destination, status=getStatusWithName('NOT Passed'),
+                       order_comment=comment, rrp=rrp, tax_rate=taxRate, exchange_rate=exchangeRate,
+                       ship_rate=shipRate, reward=reward)
+
+    return  redirect(URL('list'))
--- models/db1.py
+++ models/db1.py
@@ -1,5 +1,5 @@
 db.define_table('order_status',
-                Field('status')
+                Field('name')
                 )
 
 db.define_table('reward_points',
@@ -7,22 +7,27 @@ db.define_table('reward_points',
                 Field('ratio', 'double')
                 )
 
+db.define_table('orde_status',
+                Field('name'),
+                )
+
 db.define_table('my_order',
                 Field('item'),
                 Field('quantity'),
-                Field('gift'),
-                Field('buyer'),
-                Field('recipient'),
+                Field('gifts_info'),
+                Field('customer_name'),
+                Field('recipient_name'),
                 Field('ship_date', 'date'),
                 Field('receive_date', 'date'),
                 Field('ship_compony'),
-                Field('ship_number'),
+                Field('tracking'),
                 Field('order_destination'),
                 Field('status','reference order_status'),
                 Field('order_comment'),
                 Field('rrp', 'double'),
                 Field('tax_rate', 'double'),
-                Field('ship_fee', 'double'),
+                Field('exchange_rate', 'double'),
+                Field('ship_rate', 'double'),
                 Field('price', 'double'),
                 Field('reward','reference reward_points')
                )
@@ -31,7 +36,13 @@ if db(db.reward_points).isempty():
     db.reward_points.insert(name='Regular', ratio= 0.05)
     db.reward_points.insert(name='Double Points', ratio= 0.1)
 
-
+if db(db.order_status).isempty():
+    db.order_status.insert(name='NOT Passed')
+    db.order_status.insert(name='Passed')
+    db.order_status.insert(name='Delivered')
+    db.order_status.insert(name='Confirmed')
+    db.order_status.insert(name='Complete')
+    db.order_status.insert(name='Canceled')
 
 def selectAllOrder():
     query = (db.my_order.id>0)
@@ -40,3 +51,11 @@ def selectAllOrder():
 def selectAllRewardOptions():
     query = (db.reward_points.id>0)
     return db(query).select()
+
+def getRewardWithPercentage(ratio):
+    query = (db.reward_points.ratio==ratio)
+    return db(query).select()[0]
+
+def getStatusWithName(name):
+    query = (db.order_status.name==name)
+    return db(query).select()[0]
